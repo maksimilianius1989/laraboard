5112647392c1e69e1a301707e90f8256127daa02
Maks <it.vimax@gmail.com>
2018-07-13 07:06:30 +0300

    less: 4.2, time: 1:35, com: 08.03.18-16:17 fix bug v.3

--- resources/views/admin/regions/_nav.blade.php
+++ resources/views/admin/regions/_nav.blade.php
@@ -1,11 +1,11 @@
 <ul class="nav nav-tabs mb-3">
     <li class="nav-item">
-        <a href="{{route('admin.home')}}" class="nav-link">Dashboard</a>
+        <a class="nav-link" href="{{ route('admin.home') }}">Dashboard</a>
     </li>
     <li class="nav-item">
-        <a href="{{route('admin.regions.index')}}" class="nav-link">Regions</a>
+        <a href="{{route('admin.regions.index')}}" class="nav-link active">Regions</a>
     </li>
     <li class="nav-item">
-        <a href="{{route('admin.users.index')}}" class="nav-link">Users</a>
+        <a class="nav-link" href="{{ route('admin.users.index') }}">Users</a>
     </li>
 </ul>
