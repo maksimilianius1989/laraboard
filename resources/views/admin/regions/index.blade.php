2debb4c44d27d82d2fbcd56e2c138633f3e2f30b
Maks <it.vimax@gmail.com>
2018-07-13 07:03:33 +0300

    less: 4.2, time: 1:35, com: 08.03.18-16:17 fix bug

--- resources/views/admin/regions/index.blade.php
+++ resources/views/admin/regions/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('admin.regions._nav')
 
-    <p><a href="{{route('admin.regions.create')}}" class="btn btn-success">Add Region</a></p>
+    <p><a href="{{ route('admin.regions.create') }}" class="btn btn-success">Add Region</a></p>
 
-    @include('admin.regions._list'. ['regions' => $regions])
+    @include('admin.regions._list', ['regions' => $regions])
 @endsection
