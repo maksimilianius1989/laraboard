b4ffa9b7ffd92f4cd5e16b7415e713cd95f97105
Maks <it.vimax@gmail.com>
2018-07-22 16:25:07 +0300

    less: 4.3, time: 0:00, com: 10.03.18-18:00

---
+++ resources/views/admin/adverts/categories/attributes/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('admin.adverts.categories._nav')
+
+    <form method="POST" action="{{ route('admin.adverts.categories.attributes.update', [$category, $attribute]) }}">
+        @csrf
+        @method('PUT')
+
+        <div class="form-group">
+            <label for="name" class="col-form-label">Name</label>
+            <input type="text" id="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name', $attribute->name) }}" required>
+            @if($errors->has('name'))
+                <span class="invalid-feedback"><strong>{{ $errors->first('name') }}</strong></span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="name" class="col-form-label">Sort</label>
+            <input type="text" id="name" class="form-control{{ $errors->has('sort') ? ' is-invalid' : '' }}" name="name" value="{{ old('sort', $attribute->sort) }}" required>
+            @if($errors->has('sort'))
+                <span class="invalid-feedback"><strong>{{ $errors->first('sort') }}</strong></span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="name" class="col-form-label">Type</label>
+            <select name="type" id="type" class="form-control{{ $errors->has('type') ? ' is-invalid' : '' }}">
+                @foreach($types as $type => $label)
+                    <option value="{{ $type }}"{{ $type == old('type', $attribute->$type) ? ' selected' : '' }}>{{ $label }}</option>
+                @endforeach
+            </select>
+            @if($errors->has('type'))
+                <span class="invalid-feedback"><strong>{{ $errors->first('type') }}</strong></span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="name" class="col-form-label">Variants</label>
+            <input type="text" id="name" class="form-control{{ $errors->has('sort') ? ' is-invalid' : '' }}" name="name" value="{{ old('variants', implode("\n", $attribute->variants)) }}" required>
+            @if($errors->has('variants'))
+                <span class="invalid-feedback"><strong>{{ $errors->first('variants') }}</strong></span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <input type="hidden" name="required" value="0">
+            <div class="checkbox">
+                <label for="">
+                    <input type="checkbox" name="required" {{ old('required') ? 'checked' : '' }}>Required
+                </label>
+            </div>
+            @if($errros->has('required'))
+                <span class="invalid-feedback"><strong>{{ $errors->first('required') }}</strong></span>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <button class="btn btn-primary">Save</button>
+        </div>
+    </form>
+
+@endsection
