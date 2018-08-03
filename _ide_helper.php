4b40246821d129c266f2288d762411ab0464f475
Maks <it.vimax@gmail.com>
2018-08-03 19:52:13 +0300

    less: 5, time: 5:03, com: 18.03.18-22:57

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.28 on 2018-07-24 17:59:05.
+ * Generated for Laravel 5.6.29 on 2018-08-03 16:47:51.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
